1b117ba671c6199a119f55fe5d7f166fb4da2d71
andreastollmar <andreastollmar@gmail.com>
2023-05-18 11:29:42 +0200

    Add project files.

---
+++ Controllers/StreamingServiceController.cs
@@ -0,0 +1,41 @@
+﻿using DigitalAfterlifeApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalAfterlifeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StreamingServiceController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<StreamService>> GetStreamServicesAsync()
+        {
+            return await DAL.StreamingServiceManager.GetStreamList();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<StreamService> GetOneStreamService(int id)
+        {
+            return await DAL.StreamingServiceManager.GetOneStreamService(id);
+        } 
+
+        [HttpPost]
+        public async Task AddStreamingService([FromBody]StreamService service)
+        {
+            await DAL.StreamingServiceManager.AddStreamingService(service);
+        }
+
+        [HttpPut("{id}")]
+        public async Task UpdateStreamingservice(int id, [FromBody] StreamService service)
+        {
+            await DAL.StreamingServiceManager.UpdateStreamService(service, id);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task DeleteStreamingService(int id)
+        {
+            await DAL.StreamingServiceManager.RemoveStreamingService(id);
+        }
+    }
+}
---
+++ DAL/StreamingServiceManager.cs
@@ -0,0 +1,74 @@
+﻿using DigitalAfterlifeApi.Models;
+
+namespace DigitalAfterlifeApi.DAL
+{
+    public class StreamingServiceManager
+    {
+        public static List<StreamService> StreamServices { get; set; }
+        public static int IdCounter { get; set; }
+
+        public static async Task<List<StreamService>> GetStreamList()
+        {
+            if (StreamServices == null)
+            {
+                StreamServices = new List<StreamService>();
+                StreamServices.Add(new StreamService { Id = 1, Name = "Netflix" });
+                StreamServices.Add(new StreamService { Id = 2, Name = "Viaplay" });
+                StreamServices.Add(new StreamService { Id = 3, Name = "Disney Plus" });
+                StreamServices.Add(new StreamService { Id = 4, Name = "HBO Max" });
+                IdCounter = 5;
+            }
+            return StreamServices;
+        }
+
+        public static async Task<StreamService> GetOneStreamService(int id)
+        {
+            if (StreamServices == null)
+            {
+                await GetStreamList();
+            }
+
+            var existingService = StreamServices.Where(x => x.Id == id).SingleOrDefault();
+
+            if (existingService != null)
+            {
+                return existingService;
+            }
+            else
+            {
+                return null;
+            }
+
+
+        }
+
+        public static async Task AddStreamingService(StreamService service)
+        {
+            if (StreamServices == null)
+            {
+                StreamServices = new List<StreamService>();
+            }
+
+            service.Id = IdCounter;
+            IdCounter++;
+            service.IsChecked = null;
+            StreamServices.Add(service);
+        }
+
+        public static async Task UpdateStreamService(StreamService service, int id)
+        {
+            var existingService = StreamServices.Where(x => x.Id == id).FirstOrDefault();
+            if (existingService != null)
+            {
+                existingService.Name = service.Name;
+            }
+        }
+
+
+        public static async Task RemoveStreamingService(int id)
+        {
+            StreamServices.RemoveAt(id);   
+        }
+
+    }
+}
